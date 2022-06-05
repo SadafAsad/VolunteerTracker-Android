5d97d6caca5dfff5644fb70f0014f6097deba1b1
SadafAsad <sadaf98x@gmail.com>
2022-06-05 11:08:26 -0400

    bug fix

--- app/src/main/java/com/example/views/HistoryDetailActivity.java
+++ app/src/main/java/com/example/views/HistoryDetailActivity.java
@@ -25,7 +25,7 @@ public class HistoryDetailActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_volunteered);
 
 //        Need volunteer
 
