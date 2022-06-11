186c746c9aaafbef551d3c423082bc79104fea0e
SadafAsad <sadaf98x@gmail.com>
2022-06-11 10:28:11 -0400

    getEvents added to EventRep

--- app/src/main/java/com/example/repositories/EventRepository.java
+++ app/src/main/java/com/example/repositories/EventRepository.java
@@ -3,14 +3,20 @@ package com.example.repositories;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 
 import com.example.models.Event;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EventRepository {
@@ -25,38 +31,41 @@ public class EventRepository {
     private final String FIELD_FINISH_TIME = "finish_time";
     public String loggedInUserEmail = "";
     private final String TAG = this.getClass().getCanonicalName();
+    public MutableLiveData<List<Event>> allEvents = new MutableLiveData<>();
 
     public EventRepository() {
         DB = FirebaseFirestore.getInstance();
     }
 
-    public void addEvent(Event newEvent) {
-        try {
-            Map<String, Object> data = new HashMap<>();
-            data.put(FIELD_NAME, newEvent.getName());
-            data.put(FIELD_DETAIL, newEvent.getDetail());
-            data.put(FIELD_ORGANIZATION, newEvent.getOrganization());
-            data.put(FIELD_LOCATION, newEvent.getLocation());
-            data.put(FIELD_DATE, newEvent.getDate());
-            data.put(FIELD_START_TIME, newEvent.getStart_time());
-            data.put(FIELD_FINISH_TIME, newEvent.getFinish_time());
 
-            DB.collection(COLLECTION_EVENTS).add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                @Override
-                public void onSuccess(DocumentReference documentReference) {
-                    Log.e(TAG, "onSuccess: Document successfully created with ID " + documentReference.getId());
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.e(TAG, "onFailure: Error while creating document" + e.getLocalizedMessage());
-                }
-            });
+    public void getEvents() {
+        try{
+            DB.collection(COLLECTION_EVENTS)
+                    .orderBy(FIELD_NAME, Query.Direction.ASCENDING)
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            List<Event> eventList = new ArrayList<>();
 
-        } catch (Exception e) {
-            Log.e(TAG, "addEvent: " + e.getLocalizedMessage());
+                            if (queryDocumentSnapshots.isEmpty()){
+                                Log.e(TAG, "onSuccess: No events");
+                            }else{
+                                Log.e(TAG, "onSuccess: queryDocumentSnapshots" + queryDocumentSnapshots.getDocumentChanges() );
+
+                                for(DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()){
+                                    Event currentEvent = documentChange.getDocument().toObject(Event.class);
+                                    currentEvent.setId(documentChange.getDocument().getId());
+                                    eventList.add(currentEvent);
+                                    Log.e(TAG, "onSuccess: currentEvent " + currentEvent.toString() );
+                                }
+
+                                allEvents.postValue(eventList);
+                            }
+                        }
+                    });
+        }catch (Exception ex){
+            Log.e(TAG, "getEvents: " + ex.getLocalizedMessage() );
         }
     }
-
-    public void getEvents() {}
 }
